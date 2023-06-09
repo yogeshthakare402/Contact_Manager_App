20db7998dc32380d7b94470b57d109340ee2cbc1
yogeshthakare402 <yogeshthakare402@gmail.com>
2023-06-09 23:35:19 +0530

    Changes in map file

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,9 +4,7 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 function Map({ countryMap }) {
     const position = [51.505, -0.09]
 
-    countryMap.map((data) => {
-
-    })
+    
     return (
         <div className='mapData'>
             <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
